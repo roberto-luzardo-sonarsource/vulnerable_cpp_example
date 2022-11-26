79ae9fe4c02197288d6e49aa4b6b807d8a7c774c
HishamParveezBavest <hisham.parveez@bavest.co>
2022-11-26 20:42:29 +0100

    Memset

--- MemsetCode.c
+++ MemsetCode.c
@@ -1,15 +1,16 @@
 #define  __STDC_WANT_LIB_EXT1__ 1
 #include<string.h>
 
-/*
+
 void non(char* password, size_t bufferSize) {
-    char localToken[256];
-    init(localToken, password);
+    char *localToken;
+    localToken = *password;
     memset(password, ' ', strlen(password)); // Noncompliant, password is about to be freed
     memset(localToken, ' ', strlen(localToken)); // Noncompliant, localToken is about to go out of scope
     free(password);
 }
 
+#ifdef __STDC_LIB_EXT1__
 void com(char* password, size_t bufferSize) {
     char localToken[256];
     init(localToken, password);
@@ -17,5 +18,5 @@ void com(char* password, size_t bufferSize) {
     memset_s(localToken, sizeof(localToken), ' ', strlen(localToken));
     free(password);
 }
+#endif
 
-*/
