f161052c9fd66fa77c6ef85d8a0b73b768cf4910
HishamParveezBavest <hisham.parveez@bavest.co>
2022-11-27 15:16:36 +0100

    SonarCube parser

---
+++ parsers.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include<xercesc/parsers/XercesDOMParser.hpp>
+
+using namespace std;
+using namespace xercesc;
+
+const char* xmlFile = "samples/person.xml";
+
+void non_compliant_parser(const char* xmlFile)
+{
+	XercesDOMParser* parser = new XercesDOMParser();
+
+	parser->setCreateEntityReferenceNodes(false); // Noncompliant
+	parser->setDisableDefaultEntityResolution(false); // Noncompliant
+
+	parser->parse(xmlFile);
+}
+
+void compliant_parser(const char* xmlFile)
+{
+	XercesDOMParser* parser = new XercesDOMParser();
+
+	parser->setCreateEntityReferenceNodes(true); // Compliant: explicitly make the parser safe to XXE vulnerability
+	parser->setDisableDefaultEntityResolution(true); // Compliant
+
+	parser->parse(xmlFile);
+}
+
+
+
+
+
+
+
