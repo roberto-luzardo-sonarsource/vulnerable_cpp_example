420493dedad1d985422526c041c135c048fa780a
Hisham Parveez <hisham.parveez@bavest.co>
2023-01-29 20:27:20 +0100

    fix2

--- src/main.cpp
+++ src/main.cpp
@@ -7,36 +7,18 @@
 using namespace std;
 
 int main(int argc, char* argv[]) {
-  int num = argc - 1;
-
-  if (num == 0) {
-    cout << "No arguments provided\n";
-  } else if (num == 0) { // intentional mistake
-    cout << "1 argument provided\n";
-  } else if (num == 2) {
-    cout << "2 arguments provided\n";
-  } else {
-    cout << num << " arguments provided\n";
-  }
-  if (argv != 0) {
-    cout << "argv not null\n";; // intentional extra-semicolon
-  }
-  if (argv == nullptr) {
-    return **argv; // intentional nullptr dereference
-  }
-
   return 0;
 }
 
-void nonComplaintCode() 
+/*void nonComplaintCode()
 {
     char array[10];
     void const* pos = memchr(array, '@', 42);
-}
+}*/
 
 void ComplaintCode()
 {
-    char array[10];
+    char array[10] = "stemcells";
     void const* pos = memchr(array, '@', 10);
 }
 
--- tlsProtocol.cpp
+++ tlsProtocol.cpp
@@ -18,23 +18,23 @@ class Client_Credentials : public Botan::Credentials_Manager
 
 void protocol_main() 
 {
-    // Non-complient code
+
     Callbacks* callbacks;
     Botan::AutoSeeded_RNG rng;
     Botan::TLS::Session_Manager_In_Memory session_mgr(rng);
     Client_Credentials creds;
-    Botan::TLS::Policy policy; // Noncompliant: Default Policy has TLSv1.0 and DLTv1.0 as minimal versions
+    /*Botan::TLS::Policy policy;
 
 
-    // open the tls connection
+
     Botan::TLS::Client client(*callbacks, session_mgr, creds, policy, rng,
         Botan::TLS::Server_Information("example.com", 443),
-        Botan::TLS::Protocol_Version::TLS_V12);
+        Botan::TLS::Protocol_Version::TLS_V12);*/
 
-    //Compliant code
-    Botan::TLS::Strict_Policy new_policy; // Compliant: Strict_Policy has TLSv1.2 and TLSv1.2 as minimal versions
 
-    // open the tls connection
+    Botan::TLS::Strict_Policy new_policy; 
+
+
     Botan::TLS::Client new_client(*callbacks, session_mgr, creds, new_policy, rng,
         Botan::TLS::Server_Information("example.com", 443),
         Botan::TLS::Protocol_Version::TLS_V12);
